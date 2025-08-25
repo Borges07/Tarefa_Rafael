0b386881e1e1afbe4d67fc9ac8ba746c4cde9e17
Borges07 <matheusborgesneves07@gmail.com>
2025-08-25 19:23:52 -0300

    fix: Corrige o título da página na tag title

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 const citacoes = [
   "A imaginação é mais importante que o conhecimento. - Albert Einstein",
-  "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon",
   "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill"
 ];
 
 const citacaoEl = document.getElementById("citacao");
 const botaoEl = document.getElementById("novaCitacaoBtn");
 
-citacaoEl.style.transition = "opacity 0.5s ease";
-
 botaoEl.addEventListener("click", () => {
-  citacaoEl.style.opacity = 0; 
-
-  setTimeout(() => {
-    const randomIndex = Math.floor(Math.random() * citacoes.length);
-    citacaoEl.textContent = citacoes[randomIndex];
-    citacaoEl.style.opacity = 1; 
-  }, 500); 
+  const randomIndex = Math.floor(Math.random() * citacoes.length);
+  citacaoEl.textContent = citacoes[randomIndex];
 });
